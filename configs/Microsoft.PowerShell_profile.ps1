663842ae7ff921f9c4ce76bda1917578df5824f9
Rose Trindade <rosemtrindade@gmail.com>
2024-09-09 14:59:29 -0300

    update pwsh profile

--- configs/Microsoft.PowerShell_profile.ps1
+++ configs/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
-oh-my-posh init pwsh --config 'C:\Users\Rose\scoop\apps\oh-my-posh\23.6.0\themes\atomic.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\Rose\scoop\apps\oh-my-posh\23.11.1\themes\night-owl.omp.json' | Invoke-Expression
 Import-Module -Name Terminal-Icons
 oh-my-posh init pwsh | Invoke-Expression
